58e25d4f5ca6e19404cda4b8ac8ed65cc52af991
Tomas Zuber <tomas.zuber@external.erstegroup.com>
2024-12-02 06:24:07 +0100

    Day1 puzzle solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,33 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+fun part1(input: List<String>): Int {
+    val inputs = parse(input)
+    val list1 = inputs.first.sorted()
+    val list2 = inputs.second.sorted()
+    val distances = list1.zip(list2) { a, b -> (a - b).absoluteValue }
+    return distances.sum()
+}
+
+fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
+    val result = Pair(mutableListOf<Int>(), mutableListOf<Int>())
+    for (e in input) {
+        val values = e.split("   ")
+        result.first += values[0].toInt()
+        result.second += values[1].toInt()
+    }
+    return result
+}
+
+fun part2(input: List<String>): Int {
+    val inputs = parse(input)
+    val map = inputs.second.groupBy { it }.mapValues { it.value.size }
+    val scores = inputs.first.map { map.getOrDefault(it, 0) * it }
+    return scores.sum()
 }
