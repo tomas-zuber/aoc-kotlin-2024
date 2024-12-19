b1c50354381f5ff7ee773675e9db99b34e6394f2
Tomas Zuber <tomas.zuber@external.erstegroup.com>
2024-12-19 12:41:35 +0100

    Day12 finished part2

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,6 +1,6 @@
 fun main() {
-    val input2 = readInput("Day12")
-    val input = (
+    val input = readInput("Day12")
+    val input2 = (
             "RRRRIICCFF\n" +
             "RRRRIICCCF\n" +
             "VVRRRCCFFF\n" +
@@ -12,7 +12,7 @@ fun main() {
             "MIIISIJEEE\n" +
             "MMMISSJEEE").split("\n")
     val startTime = System.currentTimeMillis()
-    // Day12.part1(input).println()
+    Day12.part1(input).println()
     Day12.part2(input).println()
 
     println("Runtime " + (System.currentTimeMillis() - startTime))
@@ -32,7 +32,7 @@ class Day12 {
             processable.add(Point(0, 0))
             while (processable.isNotEmpty()) {
                 val type = grid.valueAt(processable.first())
-                val garden = Garden(type)
+                val garden = Garden(type, grid)
                 gardenMap[type] = (gardenMap[type] ?: mutableListOf()) + garden
                 scanGarden(processable.first(), grid, garden)
             }
@@ -66,14 +66,26 @@ class Day12 {
         }
 
         fun part2(input: List<String>): Int {
-            return 0
+            val grid = Grid(input)
+            grid.print()
+
+            processable.add(Point(0, 0))
+            while (processable.isNotEmpty()) {
+                val type = grid.valueAt(processable.first())
+                val garden = Garden(type, grid)
+                gardenMap[type] = (gardenMap[type] ?: mutableListOf()) + garden
+                scanGarden(processable.first(), grid, garden)
+            }
+
+            gardenMap.values.forEach { println("$it") }
+            return gardenMap.values.flatten().sumOf { it.sides() * it.size() }
         }
     }
 }
 
 var globalId = 0
 
-data class Garden(val name: Char) {
+data class Garden(val name: Char, val grid: Grid) {
     val id: Int = globalId++
     val points: MutableList<Point> = mutableListOf()
     var borders = 0
@@ -85,6 +97,44 @@ data class Garden(val name: Char) {
     fun size() = points.size
 
     override fun toString(): String {
-        return "Garden($name ${size()} x $borders  id=$id, points=$points)"
+        return "Garden($name ${size()} x ${sides()} /$borders id=$id, points=$points)"
     }
+
+    fun sides(): Int {
+        val xGroup = points.groupBy { it.x }
+        val yGroup = points.groupBy { it.y }
+
+        val leftEdges = getEdges(xGroup, Point::left).values
+        val rightEdges = getEdges(xGroup, Point::right).values
+        val topEdges = getEdges(yGroup, Point::up).values
+        val bottomEdges = getEdges(yGroup, Point::down).values
+        // println("L edge: ${leftEdges.size} $leftEdges")
+        // println("R edge: ${rightEdges.size} $rightEdges")
+        // println("T edge: ${topEdges.size} $topEdges")
+        // println("B edge: ${bottomEdges.size} $bottomEdges")
+
+        return con(leftEdges, Point::y) + con(rightEdges, Point::y) + con(topEdges, Point::x) + con(bottomEdges, Point::x)
+    }
+
+    private fun con(edges: Collection<List<Point>>, fn: (Point) -> Int) = edges.sumOf { points -> countSteps(points.map { fn(it) }.sorted()) }
+
+    private fun countSteps(sorted: List<Int>) : Int {
+        var result = 0
+        var lastVal:Int? = null
+        for (i in sorted.indices) {
+            if (sorted[i] - 1 != lastVal) {
+                result++
+            }
+            lastVal = sorted[i]
+        }
+        return result//.also { println("sorted $sorted -> $it") }
+    }
+
+    private fun getEdges(xGroup: Map<Int, List<Point>>, moveFn: (Point) -> Point ) =
+        xGroup.mapValues { (_, points) ->
+            points.filter {
+                val side = moveFn(it)
+                !grid.isValid(side) || grid.valueAt(it) != grid.valueAt(side)
+            }
+        }.filterValues { it.isNotEmpty() }
 }
