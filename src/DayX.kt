785cd3b184b136c9505eed4ea01800509806752e
Tomas Zuber <tomas.zuber@external.erstegroup.com>
2024-12-23 12:32:24 +0100

    Updated Day template

--- src/DayX.kt
+++ src/DayX.kt
@@ -1,7 +1,5 @@
 fun main() {
-//    val input = readInput("DayX")[0]
-    val input = "RRRRIICCFF\n" +
-            "RRRRIICCCF\n".split("\n").toList()
+    val input = readInput("DayX")
     val startTime = System.currentTimeMillis()
     DayX.part1(input).println()
     DayX.part2(input).println()
@@ -12,11 +10,11 @@ fun main() {
 class DayX {
     companion object {
 
-        fun part1(input: String): Int {
+        fun part1(input: List<String>): Int {
             return 0
         }
 
-        fun part2(input: String): Int {
+        fun part2(input: List<String>): Int {
             return 0
         }
 
